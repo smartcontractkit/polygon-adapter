5102e7f3078091e0615ca33548748f777a31004d
Thomas Hodges <me@thodges.com>
2020-02-20 07:37:46 -0600

    Add timeout param to options

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const rp = require('request-promise')
 const retries = process.env.RETRIES || 3
 const delay = process.env.RETRY_DELAY || 1000
+const timeout = process.env.TIMEOUT || 1000
 
 const requestRetry = (options, retries) => {
   return new Promise((resolve, reject) => {
@@ -51,6 +52,7 @@ const createRequest = (input, callback) => {
     url: url,
     qs: queryObj,
     json: true,
+    timeout,
     resolveWithFullResponse: true
   }
   requestRetry(options, retries)
